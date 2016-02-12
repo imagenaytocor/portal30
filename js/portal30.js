3d55d7ee238bd129a06b9e633d502cf28fc7bf6c
jf.fernandez <jf.fernandez@coruna.es>
2016-02-12 13:43:58 +0100

    Color para EIDUS
    Unificamos colores de Participación, CMIX y EIDUS

--- js/portal30.js
+++ js/portal30.js
@@ -1,5 +1,5 @@
 /**
-* @license 
+* @license
 * portal30.js - dgenerico
 */
 
@@ -10,70 +10,70 @@ $(function(){
 	$("#idiomas li:has(a)").addClass("activo");
 	$('#buscadorCabecera').find('.botonEnviarBuscador').attr('value','');
 	//$('.accesosPortada .imagen').each(function () {
-		//$(this).next().prepend(this);	
-	//});	
+		//$(this).next().prepend(this);
+	//});
 	$('.cajaDesplegable').find('h3').nextAll().not('div').wrapAll('<div class="relacionadosItem" />');
-	
+
 	$(".cajaDesplegable").find(".imagen").on("click", function (){
-	$(this).next().find(".relacionadosItem").slideToggle(); 
+	$(this).next().find(".relacionadosItem").slideToggle();
 	});
 	$(".cajaDesplegable").find("h3").on("click", function (){
-	$(this).nextAll("div.relacionadosItem").slideToggle(); 
+	$(this).nextAll("div.relacionadosItem").slideToggle();
 	});
 	$(".cajaDesplegable").find("h3").on("click", function (){
-	$(this).nextAll("ul.listaCategorias").slideToggle(); 
+	$(this).nextAll("ul.listaCategorias").slideToggle();
 	});
 	$(".cajaDesplegable").find("img").on("click", function (){
-	$(this).next().find(".listadoResultados").slideToggle(); 
+	$(this).next().find(".listadoResultados").slideToggle();
 	});
 	$(".cajaDesplegable").find("h2").on("click", function (){
-	$(this).nextAll("dl.relacionadosItem").slideToggle(); 
+	$(this).nextAll("dl.relacionadosItem").slideToggle();
 	});
 	$(".cajaDesplegable").find("img").on("click", function (){
 	$(this).addClass("imagenDesplegable");
-	$(this).next("dl").slideToggle(); 
+	$(this).next("dl").slideToggle();
 	});
 	$(".cajaDesplegable").find("h2").on("click", function (){
 	$(this).nextAll("dl").addClass("dlDesplegar");
-	$(this).nextAll("dl").slideToggle(); 
+	$(this).nextAll("dl").slideToggle();
 	});
 	$('.cajaDesplegable').find('h3').nextAll().not('div').wrapAll('<div class="relacionadosItem" />');
-	
-	
+
+
 	$('#buscadorFormularioFechas').find('p').toggleClass('abierto');
 	$('#buscadorFormularioFechas').find('p').on("click", function (){
-		$(this).next("fieldset").slideToggle(); 
+		$(this).next("fieldset").slideToggle();
 		$(this).toggleClass('abierto');
 	});
-	
+
 	$('.buscadorCategoriasComplejo ul.cat1nivel > li > input').on("click", function (){
 		$('li.catMarcada').removeClass('catMarcada');
 		$('label.abierto').removeClass('abierto').nextAll('ul').slideToggle();
-	});		
-	
+	});
+
 	$('.buscadorCategoriasComplejo ul.cat2nivel > li').has('ul').children('label').addClass('padrecito');
-	
+
 	$('.buscadorCategoriasComplejo ul.cat2nivel > li').find('.padrecito').on("click", function (){
 		$(this).toggleClass('abierto');
 		$(this).parent('li').children('ul').slideToggle();
-		return false; 
+		return false;
 	});
-	
+
 	$('.buscadorCategoriasComplejo ul.cat2nivel > li > input').on("click", function (){
 		$('li.catMarcada').removeClass('catMarcada');
 		$('label.abierto').removeClass('abierto').nextAll('ul').slideToggle();
 		$(this).parentsUntil('ul.cat1nivel > li', 'li').addClass('catMarcada');
 		$(this).next('label').toggleClass('abierto');
-		$(this).nextAll('ul').slideToggle();	
+		$(this).nextAll('ul').slideToggle();
 	});
-	
+
 	//barraCompartir
 	$('#barraCompartir').find('li.compartir > a').on("click", function (){
 		$(this).toggleClass('abierto');
 		$(this).next('ul').slideToggle();
-		return false; 
+		return false;
 	});
-	
+
 	//relacionados conc
 	$('.concDesplegable').find('.desConTitulo').each(function () {
 		$(this).find('p.tituloAcordeon').removeClass('tituloAcordeon').addClass('bio').prependTo($(this).find('.relacionados'));
@@ -83,19 +83,19 @@ $(function(){
 		$(this).toggleClass('abierto');
 		$(this).prev('div').slideToggle();
 	});
-	
+
 	//meter masInfo en buscadorResultado//
-	
+
 	$('p.masInfo').each(function () {
 		var listaHermana = $(this).prev();
 		$(this).appendTo(listaHermana);
 	});
-	
+
 	//colores duplicados
 	//Cultura -> Agora, EMM
 	$('.site_1300351791176').addClass('site_1322782055091');
 	$('.site_1300351501660').addClass('site_1322782055091');
-	//MA -> Red de energia, Infoambiental 
+	//MA -> Red de energia, Infoambiental
 	$('.site_1327566821900').addClass('site_1322782134412');
 	$('.site_1332838419278').addClass('site_1322782134412');
 	//ServSoc -> COF, Drogas, Participacion
@@ -105,10 +105,13 @@ $(function(){
 	$('.site_1346721380160').addClass('site_1352255804703');
 	$('.site_1270542954892').addClass('site_1352255804703');
 	$('.site_1266187561771').addClass('site_1352255804703');
-	
+	//Participacion -> CMIX, EIDUS
+	$('.site_1414721835012').addClass('site_1240016856326');
+	$('.site_1312153719867').addClass('site_1240016856326');
+
 	$('.mason > div').addClass('brick');
 	$('.mason > div:first-of-type').removeClass('brick');
-	
+
 	var $container = $('.mason');
 	// initialize
 	$container.masonry({
@@ -116,8 +119,8 @@ $(function(){
 	  gutter: 1,
 	  itemSelector: '.brick'
 	});
-	
+
 	var msnry = $container.data('masonry');
-		
+
 	});
 	})(jQuery);
